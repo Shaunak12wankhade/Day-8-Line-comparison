d31be0d5d8bf77560e915df8dfe5b1ecd5f7a6bc
unknown <shaunak12wankhade@ggmail.com>
2021-10-12 14:38:28 +0530

    [Shaunak] Added : Line Length

---
+++ UC1Linelength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_day_8
+{
+    class UC1Linelength
+    {
+            
+                public static void LenghtOfSingleLine()
+                {
+                    Console.WriteLine("Enter co-ordinates (x1, y1): ");
+                    double x1 = double.Parse(Console.ReadLine());
+                    double y1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter co-ordinates (x2, y2): ");
+
+                    double x2 = double.Parse(Console.ReadLine());
+                    double y2 = double.Parse(Console.ReadLine());
+                    double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                    Console.WriteLine("The Length of the line is: " + length);
+
+                }
+            
+        
+    }
+}
