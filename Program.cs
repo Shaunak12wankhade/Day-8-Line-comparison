7b6c5f86be512978c7b002fedf7f8ad8593f2773
unknown <shaunak12wankhade@ggmail.com>
2021-10-12 14:26:39 +0530

    [Shaunak] Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_day_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison");
+        }
+    }
+}
